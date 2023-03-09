d0984de0b2108a7662e680e3898612a0f27d6d4c
themonster3004 <nqd5758@gmail.com>
2023-03-09 12:12:28 +0700

    123

--- Flappy Mimi/Assets/MenuScript.cs
+++ Flappy Mimi/Assets/MenuScript.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
     public void playGame() {
-        // SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        SceneManager.LoadScene("gameplay");
     }
 
     public void exitGame() {
